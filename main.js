a8d693c11892474b56de33fcd89c4a1bb34c9a8e
GiuseppeFiliberto <giuseppe.filiberto.jobs@gmail.com>
2025-02-11 15:20:47 +0100

    fix conditions order

--- main.js
+++ main.js
@@ -7,14 +7,14 @@ for (let i = 1; i <= 100; i++) {
 
     // Creare la condizione per verificare che il programma stampi Fizz se un numero è divisibile per 3, se divisibile per 5 allora stampa Buzz, se divisibile sia per 3 che per 5 allora stampa Fizz Buzz
 
-    if (i % 3 === 0) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log('Fizz Buzz');
+        
+    } else if (i % 3 === 0) {
         console.log('Fizz');
         
     } else if (i % 5 === 0) {
         console.log('Buzz');
-        
-    } else if (i % 3 === 0 && i % 5 === 0) {
-        console.log('Fizz Buzz');
         
     } else {
         console.log(i);
