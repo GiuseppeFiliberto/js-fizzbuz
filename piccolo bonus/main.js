cc1ab1b7fa7f50983f1ed64801c1913e1bce644d
GiuseppeFiliberto <giuseppe.filiberto.jobs@gmail.com>
2025-02-11 16:34:39 +0100

    add a little bonus

---
+++ piccolo bonus/main.js
@@ -0,0 +1,6 @@
+for (let i = 1; i <= 5; i++){
+    alert ('Avviso fastidioso, non pensare di chiudermi facilmente')
+    if (i === 5) {
+        alert ("Ce l'hai fatta! La determinazione premia sempre")
+    } 
+}
