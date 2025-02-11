ea6dcc6b0bf9beeb2963d38573dd1f1f0730ab71
GiuseppeFiliberto <giuseppe.filiberto.jobs@gmail.com>
2025-02-11 14:18:33 +0100

    add last condition

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
 
 for (let i = 1; i <= 100; i++) {
 
-    // Creare la condizione per verificare che il programma stampi Fizz se un numero è divisibile per 3
+    // Creare la condizione per verificare che il programma stampi Fizz se un numero è divisibile per 3, se divisibile per 5 allora stampa Buzz, se divisibile sia per 3 che per 5 allora stampa Fizz Buzz
 
     if (i % 3 === 0) {
         console.log('Fizz');
@@ -13,6 +13,12 @@ for (let i = 1; i <= 100; i++) {
     } else if (i % 5 === 0) {
         console.log('Buzz');
         
-    } 
+    } else if (i % 3 === 0 && i % 5 === 0) {
+        console.log('Fizz Buzz');
+        
+    } else {
+        console.log(i);
+        
+    }
 
 }
